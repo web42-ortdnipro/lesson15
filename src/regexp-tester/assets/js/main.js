510341aa839121dcfb363e62e644c15e1cb0e621
Anatoliy Kigel <anatoliy.kigel@gmail.com>
2022-01-26 13:19:23 +0200

    Upload Files & Source

---
+++ src/regexp-tester/assets/js/main.js
@@ -0,0 +1,31 @@
+    
+    document.addEventListener('DOMContentLoaded', function(){
+         
+        regexp.oninput = data.oninput = function(){
+
+            var pattern = regexp.value;
+
+            var text = data.value;
+
+            controlInput.pattern = pattern;
+
+            controlInput.value = text;
+
+            if(!pattern){
+                data.style.background = "";
+                return;
+            }
+
+            var answer = controlInput.checkValidity();
+            
+            result.innerHTML = String(answer).toUpperCase();
+                   
+            if(answer){
+                data.style.background = "#aaffaa";
+            }else{
+                data.style.background = "#ffaaaa";
+            }    
+
+        }
+
+    });
